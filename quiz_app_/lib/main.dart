f2366479e4f1aba5be968952463c8c5e6ac3c2a7
Elimane FOFANA <elimane.fofana@mediasoftci.net>
2021-02-12 17:00:01 +0000

    new

--- quiz_app_/lib/main.dart
+++ quiz_app_/lib/main.dart
@@ -1,8 +1,44 @@
-addNumbers(int n1, int n2) {
-  return (n1 + n2);
-}
+import 'package:flutter/material.dart';
 
 //First function executed by DART
-void main() {
-  addNumbers(1, 2);
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  var index = 0;
+
+  //Other method
+  void answerQuestion() {
+    index++;
+    print(index);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      'What \'s your favorite color?',
+      'What \'s your favorite animal?'
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('My First App')),
+        body: Column(
+          children: <Widget>[
+            Text(questions[index]),
+            //with anonymous function
+            RaisedButton(
+                child: Text('Answer 1'),
+                onPressed: () => print('Answer 1 choosen')),
+            //with anonymous function
+            RaisedButton(
+                child: Text('Answer 2'),
+                onPressed: () {
+                  print('Answer 2 choosen');
+                }),
+            //with above called function
+            RaisedButton(child: Text('Answer 3'), onPressed: answerQuestion)
+          ],
+        ),
+      ),
+    );
+  }
 }
